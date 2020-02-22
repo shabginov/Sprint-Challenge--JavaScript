3da9ad41e071321d1b7b2638ebdb19fff67f2fc9
Alexey Shabginov <a.shabginov@gmail.com>
2020-02-21 19:10:09 -0800

    Completed objects and functions assignments

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -15,20 +15,26 @@
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
-
-// What was the diet of a velociraptor?
-console.log();
+const tyrannosaurus = {name: 'tyrannosaurus', diet: 'carnivorous' , weight: '7000kg', length: '12m', period: 'Late Cretaceous'};
+console.log(tyrannosaurus.weight);
 
 // How long was a stegosaurus?
-console.log();
+const stegosaurus = {name: 'stegosaurus', diet: 'herbivorous' , weight: '2000kg', length: '9m', period: 'Late Jurassic'}
+console.log(stegosaurus.diet);
+
+// What was the diet of a velociraptor?
+const velociraptor = {name: 'velociraptor', diet: 'carnivorous' , weight: '15kg', length: '1.8m', period: 'Late Cretaceous'}
+console.log(velociraptor.diet);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+tyrannosaurus.roar = function() {
+  return 'RAWERSRARARWERSARARARRRR!'
+}
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
@@ -52,6 +58,19 @@ const graduates = [
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+graduates.forEach(function(graduate) {
+  universities.push(graduate.university);
+})
+
+universities.sort(function(a, b) {
+  if (a < b) {
+    return -1;
+  }
+  if (a > b) {
+    return 1;
+  }
+  return 0;
+}); 
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -60,7 +79,12 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
+
+
 const contactInfo = [];
+graduates.forEach(function(student) {
+  contactInfo.push(`${student.first_name} ${student.email}`);
+})
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
@@ -91,6 +115,9 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach(function(name){
+  displayNames.push(`Name: ${name.animal_name}, Scientific: ${name.scientific_name}`);
+})
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -99,7 +126,10 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+let lowCaseAnimalNames = [];
+lowCaseAnimalNames = zooAnimals.map(function(animal) {
+  return animal.animal_name.toLowerCase();
+})
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -107,7 +137,8 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+let lowPopulationAnimals = [];
+lowPopulationAnimals = zooAnimals.filter(animal => animal.population < 5);
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -115,7 +146,8 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+let populationTotal = 0;
+populationTotal = zooAnimals.reduce((acc, animal) => acc + animal.population, populationTotal);
 console.log(populationTotal);
 
 
